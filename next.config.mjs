ff5544a28c37ece434f39290502aea7d49297e2a
dominik-merdzik <dominiktm@outlook.com>
2024-08-31 02:03:15 -0400

    fixed CORS

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,28 @@ const isRaspberryPi = os.platform() === 'linux' && os.arch() === 'arm64';
 
 const nextConfig = {
   ...(isRaspberryPi && { assetPrefix: 'https://pi.dominikmerdzik.com/' }),
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // This will match all routes
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Adjust this to restrict origins if needed
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
